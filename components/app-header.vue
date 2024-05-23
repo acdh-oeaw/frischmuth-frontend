cd20fe42a1bbd8511dcddb24cfde8104c5005cf9
oliviareichl <Olivia.Reichl@oeaw.ac.at>
2024-05-23 18:01:58 +0200

    style: style header and footer

--- components/app-header.vue
+++ components/app-header.vue
@@ -1,32 +1,41 @@
 <script lang="ts" setup>
 import type { NavLinkProps } from "@/components/nav-link.vue";
+import { Separator } from "@/components/ui/separator";
 
 const t = useTranslations();
-
+const route = useRoute();
+const homeLink = { href: { path: "/" } };
 const links = computed(() => {
 	return {
-		home: { href: { path: "/" }, label: t("AppHeader.links.home") },
+		search: { href: { path: "/search" }, label: "SUCHE" },
+		explore: { href: { path: "/explore" }, label: "ENTDECKEN" },
+		about: { href: { path: "/about" }, label: "ÜBER DAS PROJEKT" },
 	} satisfies Record<string, { href: NavLinkProps["href"]; label: string }>;
 });
 </script>
 
 <template>
-	<header class="border-b">
-		<div class="container flex items-center justify-between gap-4 py-8">
+	<header class="border-b bg-frisch-marine">
+		<div class="w-full pb-4 pt-10">
 			<nav :aria-label="t('AppHeader.navigation-main')">
-				<ul class="flex items-center gap-4" role="list">
-					<li v-for="(link, key) of links" :key="key">
-						<NavLink :href="link.href">
-							{{ link.label }}
+				<ul class="container grid grid-cols-2 items-end text-frisch-orange" role="list">
+					<div class="text-2xl font-extrabold">
+						<NavLink :href="homeLink.href">
+							Digitales Archiv
+							<br />
+							Barbara Frischmuth
 						</NavLink>
-					</li>
+					</div>
+					<div v-if="route.path !== '/'" class="ml-auto flex gap-x-4 font-semibold">
+						<li v-for="(link, key, index) of links" :key="key" class="flex shrink-0 gap-x-4">
+							<Separator v-if="index > 0" class="h-5 w-0.5 bg-frisch-orange" />
+							<NavLink :href="link.href" class="hover:text-frisch-indigo">
+								{{ link.label }}
+							</NavLink>
+						</li>
+					</div>
 				</ul>
 			</nav>
-
-			<div class="flex items-center gap-4">
-				<ColorSchemeSwitcher />
-				<LocaleSwitcher />
-			</div>
 		</div>
 	</header>
 </template>
