84fc18e250ab3325ecdfd36a8f54572fdbb184cf
oliviareichl <Olivia.Reichl@oeaw.ac.at>
2024-05-23 18:00:38 +0200

    chore: configure tailwind-config and add shadcn as a component library

--- layouts/default.vue
+++ layouts/default.vue
@@ -86,7 +86,7 @@ useHead({
 </script>
 
 <template>
-	<div class="grid min-h-full grid-rows-[auto_1fr_auto]">
+	<div class="grid min-h-full grid-rows-[auto_1fr_auto] font-sans">
 		<SkipLink target-id="main-content">{{ t("DefaultLayout.skip-to-main-content") }}</SkipLink>
 
 		<AppHeader />
