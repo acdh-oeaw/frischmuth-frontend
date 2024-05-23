3795b42afe9dc16784cd3d07838702fbaa0137ce
oliviareichl <Olivia.Reichl@oeaw.ac.at>
2024-05-23 18:04:33 +0200

    style: adapt imprint service to fit in layout

--- pages/imprint.vue
+++ pages/imprint.vue
@@ -1,18 +1,16 @@
 <script lang="ts" setup>
+import { Card, CardContent } from "@/components/ui/card";
+
 defineRouteRules({
 	prerender: true,
-});
-
-const t = useTranslations();
-
-usePageMetadata({
-	title: t("ImprintPage.meta.title"),
 });
 </script>
 
 <template>
-	<MainContent class="container py-8">
-		<h1>{{ t("ImprintPage.title") }}</h1>
-		<Imprint />
+	<MainContent class="container grid grid-cols-[auto_1fr] gap-8 px-40 py-8">
+		<Card class="size-64 bg-frisch-orange-light">
+			<CardContent class="flex size-full py-4 text-xl font-bold text-white">IMPRESSUM</CardContent>
+		</Card>
+		<Imprint class="prose font-sans prose-h2:text-frisch-orange" />
 	</MainContent>
 </template>
