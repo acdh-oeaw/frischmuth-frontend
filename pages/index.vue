7ad8492b0ad7cb694be1d36d30b9e466d6e39ad8
oliviareichl <Olivia.Reichl@oeaw.ac.at>
2024-05-23 18:03:58 +0200

    chore: add sections to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,25 @@
 <script lang="ts" setup>
-import { useQuery } from "@tanstack/vue-query";
-
-defineRouteRules({
-	prerender: true,
-});
-
-const t = useTranslations();
-const { $api } = useNuxtApp();
-
-const { data, isFetching } = useQuery({
-	queryKey: ["worklist"] as const,
-	queryFn: async () => {
-		const response = await $api.api_work_preview_list({
-			queries: {
-				limit: 10,
-				offset: 0,
-			},
-		});
-		return response;
-	},
-});
-
-usePageMetadata({
-	title: t("IndexPage.meta.title"),
-});
+import { Card, CardContent } from "@/components/ui/card";
 </script>
 
 <template>
-	<MainContent class="container py-8">
-		<h1>{{ t("IndexPage.title") }}</h1>
-		<span v-if="isFetching">Loading...</span>
-		<span v-else>
-			{{ data }}
-		</span>
+	<MainContent class="container h-full py-8">
+		<div class="mx-auto flex size-full w-[850px] grid-cols-3 gap-4 text-xl font-bold">
+			<NuxtLink href="/search">
+				<Card class="h-full w-[280px] bg-frisch-orange">
+					<CardContent class="flex size-full items-center text-white">SUCHE</CardContent>
+				</Card>
+			</NuxtLink>
+			<NuxtLink href="/explore">
+				<Card class="h-full w-[280px] bg-frisch-indigo">
+					<CardContent class="flex size-full items-center text-white">ENTDECKEN</CardContent>
+				</Card>
+			</NuxtLink>
+			<NuxtLink href="/about">
+				<Card class="h-full w-[280px] bg-frisch-grey">
+					<CardContent class="flex size-full items-center text-white">ÜBER DAS PROJEKT</CardContent>
+				</Card>
+			</NuxtLink>
+		</div>
 	</MainContent>
 </template>
