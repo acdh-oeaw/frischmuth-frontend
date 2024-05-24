298a09b2e5bdc10cb3fe0b2d019c432f56452038
oliviareichl <Olivia.Reichl@oeaw.ac.at>
2024-05-24 12:00:04 +0200

    chore: add hover transition to cards

--- pages/explore.vue
+++ pages/explore.vue
@@ -6,65 +6,87 @@ import { Card } from "../components/ui/card";
 	<MainContent class="container py-8">
 		<div class="mx-auto flex size-full grid-cols-3 items-center gap-4 font-semibold">
 			<div class="mx-auto grid max-w-7xl gap-4 font-bold grid-fluid-cols-60">
-				<Card class="relative aspect-square overflow-hidden bg-frisch-orange p-4 text-white">
+				<Card
+					class="relative aspect-square overflow-hidden bg-frisch-orange p-4 text-white transition hover:scale-[105%]"
+				>
 					<span>ORTE</span>
 				</Card>
-				<Card class="relative aspect-square overflow-hidden bg-frisch-grey p-4 text-frisch-indigo">
+				<Card
+					class="relative aspect-square overflow-hidden bg-frisch-grey p-4 text-frisch-indigo transition hover:scale-[105%]"
+				>
 					<span class="relative">Istanbul</span>
 				</Card>
 				<Card
-					class="relative aspect-square overflow-hidden bg-frisch-orange-light p-4 text-frisch-orange"
+					class="relative aspect-square overflow-hidden bg-frisch-orange-light p-4 text-frisch-orange transition hover:scale-[105%]"
 				>
 					<span>"Und dann fiel ich aus den Wolken"</span>
 				</Card>
-				<Card class="relative aspect-square overflow-hidden bg-frisch-orange p-4 text-white">
+				<Card
+					class="relative aspect-square overflow-hidden bg-frisch-orange p-4 text-white transition hover:scale-[105%]"
+				>
 					<span>FORSCHUNGS-HINSICHTEN</span>
 				</Card>
-				<Card class="relative aspect-square overflow-hidden p-4">
+				<Card class="relative aspect-square overflow-hidden p-4 transition hover:scale-[105%]">
 					<img
 						alt=""
 						class="absolute inset-0 size-full object-cover"
 						src="/assets/images/writing_placeholder.png"
 					/>
 				</Card>
-				<Card class="relative aspect-square overflow-hidden bg-frisch-grey p-4 text-frisch-indigo">
+				<Card
+					class="relative aspect-square overflow-hidden bg-frisch-grey p-4 text-frisch-indigo transition hover:scale-[105%]"
+				>
 					<span>Aus dem Archiv</span>
 				</Card>
-				<Card class="relative aspect-square overflow-hidden bg-frisch-orange p-4 text-white">
+				<Card
+					class="relative aspect-square overflow-hidden bg-frisch-orange p-4 text-white transition hover:scale-[105%]"
+				>
 					<span>NAMEN</span>
 				</Card>
-				<Card class="relative aspect-square overflow-hidden bg-frisch-indigo p-4 text-white">
+				<Card
+					class="relative aspect-square overflow-hidden bg-frisch-indigo p-4 text-white transition hover:scale-[105%]"
+				>
 					<span>JOURNALISTISCHE REZEPTION</span>
 				</Card>
 				<Card
-					class="relative aspect-square overflow-hidden bg-frisch-orange-light p-4 text-frisch-orange"
+					class="relative aspect-square overflow-hidden bg-frisch-orange-light p-4 text-frisch-orange transition hover:scale-[105%]"
 				>
 					<span>
 						"Keine Grenze kann so dicht geschlossen werden, dass sie nicht von denen, die unbedingt
 						ihr Glück versuchen wollen, überwunden wird.
 					</span>
 				</Card>
-				<Card class="relative aspect-square overflow-hidden bg-frisch-orange p-4 text-white">
+				<Card
+					class="relative aspect-square overflow-hidden bg-frisch-orange p-4 text-white transition hover:scale-[105%]"
+				>
 					<span>GLOSSAR</span>
 				</Card>
-				<Card class="relative aspect-square overflow-hidden bg-frisch-indigo p-4 text-white">
+				<Card
+					class="relative aspect-square overflow-hidden bg-frisch-indigo p-4 text-white transition hover:scale-[105%]"
+				>
 					<span>AKADEMISCHE REZEPTION</span>
 				</Card>
 				<Card
-					class="relative aspect-square overflow-hidden bg-frisch-orange-light p-4 text-frisch-orange"
+					class="relative aspect-square overflow-hidden bg-frisch-orange-light p-4 text-frisch-orange transition hover:scale-[105%]"
 				>
 					<span>
 						"Für die meisten Schriftsteller gibt es ohnehin nur eine wirkliche Heimat, und das ist
 						die Sprache, in der sie schreiben."
 					</span>
 				</Card>
-				<Card class="relative aspect-square overflow-hidden bg-frisch-grey p-4 text-frisch-indigo">
+				<Card
+					class="relative aspect-square overflow-hidden bg-frisch-grey p-4 text-frisch-indigo transition hover:scale-[105%]"
+				>
 					<span>Postkoloniale Theorie</span>
 				</Card>
-				<Card class="relative aspect-square overflow-hidden bg-frisch-orange p-4 text-white">
+				<Card
+					class="relative aspect-square overflow-hidden bg-frisch-orange p-4 text-white transition hover:scale-[105%]"
+				>
 					<span>INTERPRETATEME</span>
 				</Card>
-				<Card class="relative aspect-square overflow-hidden bg-frisch-grey p-4 text-frisch-indigo">
+				<Card
+					class="relative aspect-square overflow-hidden bg-frisch-grey p-4 text-frisch-indigo transition hover:scale-[105%]"
+				>
 					<span>Kasanapodole</span>
 				</Card>
 			</div>
--- pages/index.vue
+++ pages/index.vue
@@ -6,17 +6,17 @@ import { Card, CardContent } from "@/components/ui/card";
 	<MainContent class="container h-full py-8">
 		<div class="mx-auto flex size-full w-[850px] grid-cols-3 gap-4 text-xl font-bold">
 			<NuxtLink href="/search">
-				<Card class="h-full w-[280px] bg-frisch-orange">
+				<Card class="h-full w-[280px] bg-frisch-orange transition hover:scale-[105%]">
 					<CardContent class="flex size-full items-center text-white">SUCHE</CardContent>
 				</Card>
 			</NuxtLink>
 			<NuxtLink href="/explore">
-				<Card class="h-full w-[280px] bg-frisch-indigo">
+				<Card class="h-full w-[280px] bg-frisch-indigo transition hover:scale-[105%]">
 					<CardContent class="flex size-full items-center text-white">ENTDECKEN</CardContent>
 				</Card>
 			</NuxtLink>
 			<NuxtLink href="/about">
-				<Card class="h-full w-[280px] bg-frisch-grey">
+				<Card class="h-full w-[280px] bg-frisch-grey transition hover:scale-[105%]">
 					<CardContent class="flex size-full items-center text-white">ÜBER DAS PROJEKT</CardContent>
 				</Card>
 			</NuxtLink>
