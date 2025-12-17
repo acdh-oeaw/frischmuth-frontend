d4cb572f56aa6caf3c7dacccdde0c41d57f80637
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2025-12-17 07:01:26 +0100

    feat: #68 add badge component

---
+++ components/ui/badge/Badge.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { reactiveOmit } from "@vueuse/core";
+import { Primitive, type PrimitiveProps } from "reka-ui";
+import type { HTMLAttributes } from "vue";
+
+import { cn } from "@/utils/styles";
+
+import { type BadgeVariants, badgeVariants } from ".";
+
+const props = defineProps<
+	PrimitiveProps & {
+		variant?: BadgeVariants["variant"];
+		class?: HTMLAttributes["class"];
+	}
+>();
+
+const delegatedProps = reactiveOmit(props, "class");
+</script>
+
+<template>
+	<Primitive
+		:class="cn(badgeVariants({ variant }), props.class)"
+		data-slot="badge"
+		v-bind="delegatedProps"
+	>
+		<slot />
+	</Primitive>
+</template>
